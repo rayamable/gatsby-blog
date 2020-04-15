af6c5220c41c09968375c9e52c74181eb1af2f5e
Ray Amable <rayamable@gmail.com>
2020-04-15 18:27:27 +0000

    add markdown pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default ({ data }) => {
 
-export default IndexPage
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div>
+        <h1>Ray's Thoughts</h1>
+        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <span>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </span>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            description
+            title
+            date
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
