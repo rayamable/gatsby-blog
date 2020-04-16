2b5f5b87ac1a539a172eeef7c83c8f2c57d9283f
Ray Amable <rayamable@gmail.com>
2020-04-16 09:51:39 +0000

    added templates and styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,37 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-export default ({ data }) => {
+const BlogLink = styled(Link)`
+  text-decoration: none;
+  &:hover {
+    color: blue;
+  }
+`
 
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+`
+
+export default ({ data }) => {
+  console.log(data)
   return (
     <Layout>
       <SEO title="Home" />
       <div>
         <h1>Ray's Thoughts</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                {node.frontmatter.title} - {node.frontmatter.date}
+              </BlogTitle>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))}
@@ -28,7 +42,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -38,9 +52,13 @@ export const query = graphql`
             title
             date
           }
+          fields {
+            slug
+          }
           excerpt
         }
       }
     }
   }
 `
+
